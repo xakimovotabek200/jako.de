4376753b8d289f7bfd171d357f53beaa670fd403
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-10-19 14:55:27 +0300

    add mantine navbar

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Kids from './components/Kids/Kids'
 import Sale from './pages/sale/Sale';
-
-
+import Men from "./pages/mens/Mens"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <NavbarTop />
+        <NavbarTop/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/kids" element={<Kids/>} />
-          <Route path="/sale" element={<Sale/>} />
+          <Route path="/kids" element={<Kids />} />
+          <Route path="/mens" element={<Men />} />
+          <Route path="/sale" element={<Sale />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
