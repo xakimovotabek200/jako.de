93c61d275dffbec11fdc6dc5c623d80bf1882d19
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-11-13 15:48:04 +0500

    add translater

--- src/components/Jakosx/Jakos.jsx
+++ src/components/Jakosx/Jakos.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import JakoOnline from "./jakoonline/JakoOnline";
 import JoinDAnPAsti from "./joindanPasti/JoinDAnPAsti";
-import Partners from "../Partners/Partners";
 
 function Jakos() {
   return (
     <div className="container">
       <JoinDAnPAsti />
-      <Partners/>
       <JakoOnline />
     </div>
   );
--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Partners = () => {
     const [partners, setPartners] = useState([]);
@@ -22,21 +23,27 @@ const Partners = () => {
 
         fetchPartners();
     }, []);
-console.log(partners);
     return (
-        <div>
+        <div className='container mx-auto'>
             {
                 Array.isArray(partners) && partners.map((item) => {
                     return (
                         <div key={item.id}>
-                            <h1>{item.name}</h1>
+                            <h1 className='text-center text-[55px] text-neutral-600 font-semibold'>Partners</h1>
+                            <div className='border-[1px] border-solid border-neutral-600 w-full mb-5' />
+                            <div>
+                                <img
+                                    className='w-[85px] h-[75px] object-cover cursor-pointer'
+                                    src={`https://api.abdullajonov.uz/legend-backend-api/public/storage/images/${item.logo}`}
+                                    alt="" />
+                            </div>
                         </div>
                     )
                 })
             }
 
         </div>
-    );      
+    );
 };
 
 export default Partners;
